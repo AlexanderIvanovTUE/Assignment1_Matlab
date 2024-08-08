fa2d8257993f68cc1d44788af85d68c1e8372abc
Alexander Ivanov <133096085+Alex16Ivanov@users.noreply.github.com>
2024-08-08 17:17:23 +0300

    Example imput data file, file to run the function and the created plot added.

---
+++ main.m
@@ -0,0 +1,8 @@
+clc,clearvars
+
+% Define the file paths
+inputFile = 'inputData.csv';          % Your input CSV file
+outputPlotFile = 'output_plot.png';   % File where the plot will be saved
+
+% Call the function
+processData(inputFile, outputPlotFile);
